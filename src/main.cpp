b44247635d0d199a721709d7a0e420e2c7e78a50
Albert Wang <albertwang@ironclad.mobi>
2013-11-06 12:47:37 -0600

    Math

--- src/main.cpp
+++ src/main.cpp
@@ -15,56 +15,13 @@
 
 #include "channel.h"
 
-class Microprofiler
-{
-public:
-	Microprofiler(const char * name)
-		:name(name)
-	{
-		gettimeofday(&start, NULL);
-	}
-
-	~Microprofiler()
-	{
-		timeval end; 
-		gettimeofday(&end, NULL); 
-
-		size_t passed = 1000 * 1000 * (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec);
-		std::cout << name << ": " << passed / 1000 << "ms\n";
-	}
-private:
-	const char * name;
-	timeval start;
-};
-
-void invoke(size_t i)
-{
-	std::stringstream s;
-	s << i << " " << i << " " << i << " " << i << " " << i << " " << i << " " << i;
-	s.flush();
-}
-
-
-void invoke(Engine::Memory::StackAllocator& alloc, size_t i)
-{
-	Engine::Memory::StackScope scope(&alloc);
-	Engine::Memory::Stream s(scope);
-	s << i << " " << i << " " << i << " " << i << " " << i << " " << i << " " << i;
-	s.flush();
-}
 
 int main(int argc, char * argv[])
 {
-	using namespace Engine::Threading;
-	Bidirectional b;
+	Engine::Memory::StackAllocator alloc(1024 * 10);
 
-	b.subscribe(&b);
-	b.send(1, 2, 3);
+	Engine::Memory::StackScope scope(&alloc, 1024);
 
-	while (Message msg = b.pop())
-	{
-		std::cout << msg.type << "\n";
-	}
-
+	std::cout << Engine::format(scope, "Hello, {0} {0} {1} {0}\n", "Hi", 42);
 	return 0;
 }
