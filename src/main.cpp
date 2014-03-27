d86135e0203eccb969327a9b84cb8f64c36fe531
Albert <albertwang@ironclad.mobi>
2014-03-27 17:56:53 -0500

    Math examples in main

--- src/main.cpp
+++ src/main.cpp
@@ -22,5 +22,7 @@ int main(int argc, char * argv[])
 	Engine::Memory::StackScope scope(&alloc);
 
 	std::cout << Engine::format(scope, "Hello, {0} {0} {1} {0}\n", "Hi", 42);
+
+	std::cout << Vector4(1, 0, 0, 1) * Matrix4::rotation(0, 0, 3.14159 / 2) << "\n";
 	return 0;
 }
